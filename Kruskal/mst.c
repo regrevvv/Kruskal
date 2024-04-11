bc521569144e530d02d5ceffb3ef1fe2458025f0
regrevvv <txr0317@gmail.com>
2024-04-12 03:06:16 +0900

    초기 커밋.

---
+++ Kruskal/mst.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+	//input 파일 열기
+	FILE* fp;
+	if (argc != 2)
+	{
+		printf("Needs one parameter - file name that contains adjacency matrix.");
+		return -1;
+	}
+
+	if ((fopen_s(&fp, argv[1], "r")) != 0)
+	{
+		perror("Failed to open file.");
+		return -1;
+	}
+
+	//텍스트에서 노드 수 읽어오기
+	int loop = 1, nodeNum = 0;
+	while (loop)
+	{
+		switch (fgetc(fp))
+		{
+		case ' ':
+			nodeNum++;
+			break;
+		case '\n':
+			nodeNum++;
+			loop = 0;
+			break;
+		}
+	}
+
+	//2차원 배열 동적 할당후 인접행렬 데이터 삽입
+	int** adj;
+	if ((adj = (int**)malloc(sizeof(int*) * nodeNum)) == NULL) {
+		perror("Memory allocate error!");
+		return -1;
+	}
+
+	if ((adj[0] = (int*)malloc(sizeof(int) * nodeNum * nodeNum)) == NULL)
+	{
+		perror("Memory allocate error!");
+		return -1;
+	}
+	
+	for (int i = 1; i < nodeNum; i++)
+		adj[i] = adj[i - 1] + 4;
+
+
+
+
+
+	free(adj[0]);
+	free(adj);
+	fclose(fp);
+	return 0;
+}
